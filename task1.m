59cebd366616bac0aba5941b26f720c81a0d8071
Salih MSA <salih.msa@outlook.com>
2023-05-08 12:25:17 +0100

    Deleting anything to do with boundary-based segmentation. It identifies the boundary but doesn't show where it cuts off, etc.. We want to group instead into regions using region-based segmentation. Alot of work wasted for nothing but there's nothing I can do

--- task1.m
+++ task1.m
@@ -10,7 +10,7 @@ load("resources/Brain.mat"); % loads data
 
 WAIT_TIME = 1;
 
-%% Task 1
+%% Calling Segmentation Algorithms
 %%% Develop and apply different segmentation algorithms, based on any
 %%% technique you have learnt to each slice of the MRI data
 %%% You need to apply exactly the same algorithm to every slice
@@ -18,6 +18,19 @@ WAIT_TIME = 1;
 %%% Note: we want 5 tissue layers (6 layers total inc. air aka our
 %%% background)
 
-% threshold_slices = segment_slices(T1, @wthreshold, WAIT_TIME); % apply Otsu's thresholding (note @ needed to pass functions)
-snake_slices = segment_slices(T1, @wsnakes, WAIT_TIME); % apply active contours
-% watershed_slices = segment_slices(T1, @wwatershed); % apply active contours
+% threshold_slices = segment_2d_slices(T1, @wthreshold, WAIT_TIME); % apply Otsu's thresholding (note @ needed to pass functions)
+
+function [modified_slices] = segment_2d_slices(original_slices, segmentation_algorithm, wait_time)
+%SEGMENT_SLICES Meta function to apply, display and return segmentated slices as a result of a supplied algorithm
+    if ~exist('wait_time','var')
+        wait_time = 0;
+    end
+
+    modified_slices = original_slices; % We create copies of the slices, so that we can modify each and cmp later
+    for i = 1:size(modified_slices, 3) % loop over all slices (image is 2d, so 1D mat of mat is 3d)
+        modified_slices(:, :, i) = segmentation_algorithm(modified_slices(:, :, i)); % overwrite old slice with result of segmentation
+        img = show_image(modified_slices(:, :, i), sprintf("Slice #%d",  i));
+        pause(wait_time);
+        close(img);
+    end
+end
