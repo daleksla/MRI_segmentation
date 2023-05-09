25296e3e18fa0d5362de5bc914eb5256f1c926c8
Salih MSA <salih.msa@outlook.com>
2023-05-09 02:17:37 +0100

    Started task2. need to implement ways to determine sensitivity + specificity

---
+++ task2.m
@@ -0,0 +1,27 @@
+%%% Task 2 %%%
+%%% Compare your segmented results for each algorithm to the ground-truth label provided
+%%% Justify and explain the metric used to assess accuracy
+%%% Based on your evaluation and results, highlight the best algorithm to be used
+
+%% Initialisation
+%%% Check that task1.m was run (or rather, that we have it's outputs)
+%%% Then load slices - we want label this time
+
+if ~exist('threshold_slices','var') || ~exist('kmeans_slices', 'var')
+    quit(1, "Segmented slices not available for analysis")
+end
+
+slices = load("resources/Brain.mat"); % load slices
+
+%% Calling evaluation algorithm
+%%% Using sensitivity & sensitivity as our metric
+
+threshold_performance = evaluate_performance(threshold_slices, label);
+kmeans_performance = evaluate_performance(kmeans_slices, label);
+
+function [average_metric] = evaluate_performance(segmented_slice, truth_slice)
+%EVALUATE_PERFORMANCE Meta function to create confusion matrix between
+%ground truth and algorithm x's segmentation result. We then extract and
+%average the sensitivity and specificity values
+    ...
+end
