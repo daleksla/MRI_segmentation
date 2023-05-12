640d3f316ff7169dafbb6d0513cd2e8439877d7a
Salih MSA <salih.msa@outlook.com>
2023-05-12 05:17:34 +0100

    added docstring

--- functions/group_performance.m
+++ functions/group_performance.m
@@ -1,12 +1,16 @@
 function [average_sensitivity, average_specificity, score_table] = group_performance(test_slices, truth_slices)
-%     if ~islogical(test_slices(:, :, 1))
-%         fprintf("Test slices bad format\n");
-%         return;
-%     elseif ~islogical(truth_slices(:, :, 1))
-%         fprintf("Truth slices bad format\n");
-%         return;
-%     end
-
+%GROUP_PERFORMANCE Function to return average sensitivity and specificity
+%metrics
+% Function calculates, for all relevant slices, each's sensitivity and
+% specificity
+% We then extract and average the sensitivity and specificity values
+%
+% test_slice - image we wish to evaluate correctness of
+% truth_slice - image to be used as absolute truth
+% average_sensitivity - calculated sensitivity metric
+% average_specificity - calculated specificity metric
+% score_table - table containing, with entry for each relevant slice block's, evaluation scores 
+%
     slice_count = size(truth_slices, 3);
 
     slice_names = 1:slice_count+1; % last one is avg
@@ -14,7 +18,6 @@ function [average_sensitivity, average_specificity, score_table] = group_perform
     specificity_ress = zeros(1, slice_count + 1);
 
     for i = 1:slice_count
-%         fprintf("Loop %d, ", i);
         [sensitivity, specificity] = evaluate_performance(...
             test_slices(:, :, i), truth_slices(:, :, i)...
         );
@@ -22,7 +25,6 @@ function [average_sensitivity, average_specificity, score_table] = group_perform
         sensitivity_ress(i) = sensitivity;
         specificity_ress(i) = specificity;
     end
-%     fprintf("\n");
 
     average_sensitivity = mean(sensitivity_ress(1:slice_count));
     sensitivity_ress(slice_count + 1) = average_sensitivity;
